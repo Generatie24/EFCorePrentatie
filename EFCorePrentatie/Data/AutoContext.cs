3c0fe73fd6223ec404d9e0a7127688db8109daf3
Kenan Kurda <intecbrussel24@gmail.com>
2024-05-13 11:39:39 +0200

    Add project files.

---
+++ EFCorePrentatie/Data/AutoContext.cs
@@ -0,0 +1,14 @@
+﻿using EFCorePrentatie.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCorePrentatie.Data
+{
+    public class AutoContext : DbContext
+    {
+        public AutoContext(DbContextOptions<AutoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Auto> Autos { get; set; }
+    }
+}
